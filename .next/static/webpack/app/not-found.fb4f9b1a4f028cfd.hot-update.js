782d1a9c86e2d1ac8e755e57ec73d4ef2cbb2e5c
Surya_Pratap <suryapratapsahu297@gmail.com>
2025-04-19 09:30:44 +0530

    Chnage HeroSection color

---
+++ .next/static/webpack/app/not-found.fb4f9b1a4f028cfd.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/not-found",{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fsuryapratapsahu%2FDesktop%2Fclone%20%2Fdiligent%2Fcomponents%2Flayout%2FLayout.js&modules=%2FUsers%2Fsuryapratapsahu%2FDesktop%2Fclone%20%2Fdiligent%2Fnode_modules%2Fnext%2Fdist%2Fclient%2Flink.js&server=false!":
+/*!*******************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fsuryapratapsahu%2FDesktop%2Fclone%20%2Fdiligent%2Fcomponents%2Flayout%2FLayout.js&modules=%2FUsers%2Fsuryapratapsahu%2FDesktop%2Fclone%20%2Fdiligent%2Fnode_modules%2Fnext%2Fdist%2Fclient%2Flink.js&server=false! ***!
+  \*******************************************************************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./components/layout/Layout.js */ \"(app-pages-browser)/./components/layout/Layout.js\"));\nPromise.resolve(/*! import() eager */).then(__webpack_require__.t.bind(__webpack_require__, /*! ./node_modules/next/dist/client/link.js */ \"(app-pages-browser)/./node_modules/next/dist/client/link.js\", 23))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz0lMkZVc2VycyUyRnN1cnlhcHJhdGFwc2FodSUyRkRlc2t0b3AlMkZjbG9uZSUyMCUyRmRpbGlnZW50JTJGY29tcG9uZW50cyUyRmxheW91dCUyRkxheW91dC5qcyZtb2R1bGVzPSUyRlVzZXJzJTJGc3VyeWFwcmF0YXBzYWh1JTJGRGVza3RvcCUyRmNsb25lJTIwJTJGZGlsaWdlbnQlMkZub2RlX21vZHVsZXMlMkZuZXh0JTJGZGlzdCUyRmNsaWVudCUyRmxpbmsuanMmc2VydmVyPWZhbHNlISIsIm1hcHBpbmdzIjoiQUFBQSxvTEFBK0c7QUFDL0ciLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLz83NmY1Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCgvKiB3ZWJwYWNrTW9kZTogXCJlYWdlclwiICovIFwiL1VzZXJzL3N1cnlhcHJhdGFwc2FodS9EZXNrdG9wL2Nsb25lIC9kaWxpZ2VudC9jb21wb25lbnRzL2xheW91dC9MYXlvdXQuanNcIik7XG5pbXBvcnQoLyogd2VicGFja01vZGU6IFwiZWFnZXJcIiAqLyBcIi9Vc2Vycy9zdXJ5YXByYXRhcHNhaHUvRGVza3RvcC9jbG9uZSAvZGlsaWdlbnQvbm9kZV9tb2R1bGVzL25leHQvZGlzdC9jbGllbnQvbGluay5qc1wiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fsuryapratapsahu%2FDesktop%2Fclone%20%2Fdiligent%2Fcomponents%2Flayout%2FLayout.js&modules=%2FUsers%2Fsuryapratapsahu%2FDesktop%2Fclone%20%2Fdiligent%2Fnode_modules%2Fnext%2Fdist%2Fclient%2Flink.js&server=false!\n"));
+
+/***/ })
+
+});
